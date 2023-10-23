98af54ec64b5daac6a3285838f9793ce6fbdac86
FropitFop <volodya.lichinka@gmail.com>
2023-10-23 22:16:41 +0300

    я сделял

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = prompt ("Твое имя?");
+alert ("Хорошо я запомню что тебя зовут "+ name);
+
+let AianamiRay;
+AianamiRay = "Кто ты?";
+AianamiRay = "Аянами Рей ";
+AianamiRay = "А кто ты ?";
+AianamiRay = "Аянами Рей ";
+alert (AianamiRay);
+
+
+const JAM = "Единственный кто может меня победить, это я сам";
+alert (JAM);
+alert (JAM ="")
+
+const year = "1954"
+console.log (typeof year)
+console.log (typeof (Number(year)))
+
